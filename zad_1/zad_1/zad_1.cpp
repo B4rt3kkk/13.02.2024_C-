e5a968746c9dbed59c6ce13b1d91f7fa056da82c
FilipZembrzuski <fzembrzuski@gmail.com>
2024-02-13 10:17:37 +0100

    c++

---
+++ zad_1/zad_1/zad_1.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+using namespace std;
+
+class Robot {
+    public:
+        string name;
+        int serial_number;
+        int digit;
+
+        void runRobot() {
+            if (digit == 1) {
+                showRobot();
+            }
+            else {
+                messages();
+            }
+        }
+
+        void showRobot() {
+            cout << "Robot uruchomiony: nr - " << serial_number << " Nazwa: "
+                << name << endl;
+        }
+        void messages() {
+            cout << "Robott nie uruchomiony" << endl;
+        }
+
+};
+
+
+int main()
+{
+    int uruchomienie;
+    cout << "Robot 1 - Start, inna wartość: Robot nie działa " << endl;
+    cout << "Co chesz zrobic: "; cin >> uruchomienie;
+    cout << "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=" << endl; 
+    Robot mojrobocik;
+    mojrobocik.digit = uruchomienie;
+    mojrobocik.serial_number = 123456789;
+    mojrobocik.name = "Bobert 1";
+    mojrobocik.runRobot();
+
+    return EXIT_SUCCESS;
+}
+
---
+++ zad_2/zad_2/zad_2.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+using namespace std;
+
+class Maszyna {
+public:
+    string name;
+    int moc;
+    int digit;
+    string color;
+
+    void runMaszyna() {
+        if (digit == 123) {
+            showMaszyna();
+        }
+        else {
+            messages();
+        }
+    }
+
+    void showMaszyna() {
+        cout << "Maszyna uruchomiona :) " << " Moc w Watach: " << moc << ", Nazwa: " << name << ", Kolor: " << color << endl;
+    }
+    void messages() {
+        cout << "Maszyna nie uruchomiony" << endl;
+    }
+
+};
+
+
+int main()
+{
+    int uruchomienie;
+    cout << "Machina - Start, inna wartość: Machina nie działa " << endl;
+    cout << "Wpisz hasło: "; cin >> uruchomienie;
+    cout << "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=" << endl;
+    Maszyna mojamaszyna;
+    mojamaszyna.digit = uruchomienie;
+    mojamaszyna.color = "czorny";
+    mojamaszyna.moc = 1000;
+    mojamaszyna.name = "Super_Kuper_Machina";
+    mojamaszyna.runMaszyna();
+
+    return EXIT_SUCCESS;
+}
+
